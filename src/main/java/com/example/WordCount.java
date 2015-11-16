b7c735bcf744d41d67354971cae53bdd9b4ace69
amallya13 <aniruddha.mallya@oracle.com>
2015-11-16 15:05:41 -0800

    New Commit #1231

--- src/main/java/com/example/WordCount.java
+++ src/main/java/com/example/WordCount.java
@@ -72,4 +72,7 @@ public class WordCount {
     FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
     System.exit(job.waitForCompletion(true) ? 0 : 1);
   }
+  /*
+   * New commit
+   */
 }
